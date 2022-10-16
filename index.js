78b3c847aba19bdac5c4daff090258078f24e72b
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jankee112@freenet.de>
2022-10-16 21:47:49 +0200

    create weeks rendering

--- index.js
+++ index.js
@@ -1,20 +1,55 @@
 const weeks = Array.from(Array(52).keys());
-// const weekBlock = weeks / 13;
+let weekslived = [];
 // let alter = prompt("gebe dein Alter ein");
 
-let alter = 18;
+let alter;
+let blockRow = document.querySelector(".block-row")
 
-const weeksRow = document.querySelector(".row");
+/**
+ * create a block of 4 Weeks in a Single Iteration
+ ** [1 Year contains 13 Blocks]
+ */
+const createBlock = () => {
+    let div = document.createElement("DIV"); // 1x
+    blockRow.insertAdjacentElement("beforeend", div) //1x
+    div.classList.add("block"); //1x
+    for (let i = 0; i < 4; i++) {
+        let span = document.createElement("SPAN"); // 4x
+        div.insertAdjacentElement("beforeend", span) //4x
+        span.classList.add("field"); //4x
+    }
+}
 
-const button = document.querySelector(".button");
-
-console.log(weeks);
-console.log(alter);
-console.log(weeksRow);
+let now = new Date()
+let bd = new Date("1992-03-31")
 
 
+/**
+ * create a Row containing 13 X 4 Weeks Block
+ */
+const createYearBlockRow = () => {
+    let faktor = 13 // 1 Year = 13 Weeks
+    let blocks = (now.getFullYear() - bd.getFullYear()) * faktor
+    let weeks = Math.round(blocks * 4)
+    for (let i = 0; i < weeks; i++) {
+        weekslived.push(i)
+    }
 
+    for (let i = 0; i < blocks; i++) {
+        createBlock()
+    }
+    fillWeeks(blocks)
+}
 
-button.addEventListener("click", () => {
-  console.info("clicked");
-});
+const fillWeeks = (blocks) => {
+    let fields = document.querySelectorAll(".field")
+
+    for (let i = 0; i <= blocks; i++) {
+
+        weekslived.forEach(week => {
+            fields[week].classList.add("filled")
+        })
+    }
+};
+
+createYearBlockRow()
