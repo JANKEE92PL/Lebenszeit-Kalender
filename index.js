4bc77a502fd9766ef6631d1a3710e25b049c7721
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jankee112@freenet.de>
2022-10-16 22:21:34 +0200

    finished logic for weeks in years

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const weeks = Array.from(Array(52).keys());
 let weekslived = [];
-// let alter = prompt("gebe dein Alter ein");
 
-let alter;
 let blockRow = document.querySelector(".block-row")
 
 /**
@@ -19,37 +17,50 @@ const createBlock = () => {
         span.classList.add("field"); //4x
     }
 }
+// let alter = new Date("1999-03-31")
+// let age = prompt("Gebe dein Alter ein >>YYYY-MM-DD<<");
+const datepicker = document.getElementById("datepicker");
+datepicker.addEventListener("input", (e) => {
+    clearFields()
+    const date = new Date(datepicker.value);
+    console.info(date.getFullYear())
+    createYearBlockRow(date)
+})
 
-let now = new Date()
-let bd = new Date("1992-03-31")
+const clearFields = () => {
+    let targets = document.querySelectorAll(".block");
+    targets.forEach((target) => {
+        blockRow.removeChild(target)
+    })
+}
 
 
 /**
  * create a Row containing 13 X 4 Weeks Block
+ * @date picked date value from Datepicker
  */
-const createYearBlockRow = () => {
+const createYearBlockRow = (date) => {
     let faktor = 13 // 1 Year = 13 Weeks
-    let blocks = (now.getFullYear() - bd.getFullYear()) * faktor
+    const now = new Date()
+    let blocks = (now.getFullYear() - date.getFullYear()) * faktor
     let weeks = Math.round(blocks * 4)
     for (let i = 0; i < weeks; i++) {
         weekslived.push(i)
     }
-
     for (let i = 0; i < blocks; i++) {
         createBlock()
     }
     fillWeeks(blocks)
 }
-
+/**
+ * fill all fields created from Funktion #createBlock with black color using css class ".filled"
+ * @param 1 Block contains 4 Weeks
+ */
 const fillWeeks = (blocks) => {
     let fields = document.querySelectorAll(".field")
-
     for (let i = 0; i <= blocks; i++) {
-
         weekslived.forEach(week => {
             fields[week].classList.add("filled")
         })
     }
 };
-
-createYearBlockRow()
